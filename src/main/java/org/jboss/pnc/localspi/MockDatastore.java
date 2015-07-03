9a88c33d5d132e6dbfc5ca698f209cd4326c3bd1
Ahmed Abu Lawi <aabulawi@redhat.com>
2015-07-03 15:33:52 -0400

    Update for upgrade to 06

--- src/main/java/org/jboss/pnc/localspi/MockDatastore.java
+++ src/main/java/org/jboss/pnc/localspi/MockDatastore.java
@@ -1,5 +1,6 @@
 package org.jboss.pnc.localspi;
 
+
 import org.jboss.pnc.model.BuildRecord;
 import org.jboss.pnc.model.User;
 import org.jboss.pnc.spi.datastore.Datastore;
@@ -9,14 +10,16 @@ import org.jboss.pnc.spi.datastore.DatastoreException;
  * Created by aabulawi on 24/06/15.
  */
 public class MockDatastore implements Datastore {
-    @Override
-    public void storeCompletedBuild(BuildRecord buildRecord) throws DatastoreException {
 
+
+    @Override
+    public BuildRecord storeCompletedBuild(BuildRecord buildRecord) throws DatastoreException {
+        return buildRecord;
     }
 
     @Override
     public User retrieveUserByUsername(String s) {
-        return null;
+        return new User();
     }
 
     @Override
