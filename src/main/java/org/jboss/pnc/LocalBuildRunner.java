839b15838df8a99b53b42c51262d28d8ed41c01c
Ahmed Abu Lawi <aabulawi@redhat.com>
2015-07-03 15:34:19 -0400

    Add shutdownhook for weld.
    Hack to end to process. For some java process doesn't end when the build is completed. I believe it is hanging due to an issue with weld.

--- src/main/java/org/jboss/pnc/LocalBuildRunner.java
+++ src/main/java/org/jboss/pnc/LocalBuildRunner.java
@@ -1,6 +1,5 @@
 package org.jboss.pnc;
 
-import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
 import org.jboss.pnc.core.builder.BuildCoordinator;
@@ -8,22 +7,59 @@ import org.jboss.pnc.core.exception.CoreException;
 import org.jboss.pnc.model.BuildConfiguration;
 import org.jboss.pnc.model.Environment;
 import org.jboss.pnc.model.User;
-import org.jboss.weld.environment.se.events.ContainerInitialized;
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
 
 public class LocalBuildRunner {
 
     @Inject
-    private BuildCoordinator buildCoordinator;
+    BuildCoordinator buildCoordinator;
+    public void run(){
 
-    public void main(@Observes ContainerInitialized event){
+        int result = 0;
         Environment env = Environment.Builder.defaultEnvironment().build();
         BuildConfiguration buildConfiguration = BuildConfiguration.Builder.newBuilder().id(0).scmRepoURL("git@github.com:release-engineering/pom-manipulation-ext.git")
-                .buildScript("mvn install").name("MYBUILDCONFIG").environment(env).build();
+                .buildScript("mvn install").name("MYBUILDCONFIG").environment(env).scmRevision("22855c9404319a30195b8ba7e637ddfb3f5dcaaf").build();
         User user = User.Builder.newBuilder().id(1).build();
+
+
         try {
             buildCoordinator.build(buildConfiguration, user);
         } catch (CoreException e) {
-            e.printStackTrace();
+            result = 1;
+        }
+
+        while(!buildCoordinator.getBuildTasks().isEmpty()){
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                result = 1;
+            }
+        }
+        System.exit(result);
+
+    }
+
+    public static void main(String[] args){
+
+        Weld weld = new Weld();
+        WeldContainer weldContainer = weld.initialize();
+        weldContainer.instance().select(LocalBuildRunner.class).get().run();
+
+        Runtime.getRuntime().addShutdownHook(new ShutdownHook(weld));
+    }
+
+    static class ShutdownHook extends Thread {
+        private final Weld weld;
+
+
+        ShutdownHook(final Weld weld) {
+            this.weld = weld;
+        }
+
+
+        public void run() {
+            weld.shutdown();
         }
     }
 }
