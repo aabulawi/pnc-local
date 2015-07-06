17388e8b42a722c897e541d8a526f655a0f929a9
Ahmed Abu Lawi <aabulawi@redhat.com>
2015-07-06 15:19:54 -0400

    Register observers to the BuildChangedStatus to determine when the build is finished and can process can be terminated.

--- src/main/java/org/jboss/pnc/LocalBuildRunner.java
+++ src/main/java/org/jboss/pnc/LocalBuildRunner.java
@@ -1,5 +1,6 @@
 package org.jboss.pnc;
 
+import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
 import org.jboss.pnc.core.builder.BuildCoordinator;
@@ -7,6 +8,8 @@ import org.jboss.pnc.core.exception.CoreException;
 import org.jboss.pnc.model.BuildConfiguration;
 import org.jboss.pnc.model.Environment;
 import org.jboss.pnc.model.User;
+import org.jboss.pnc.spi.events.BuildSetStatusChangedEvent;
+import org.jboss.pnc.spi.events.BuildStatusChangedEvent;
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
 
@@ -16,7 +19,6 @@ public class LocalBuildRunner {
     BuildCoordinator buildCoordinator;
     public void run(){
 
-        int result = 0;
         Environment env = Environment.Builder.defaultEnvironment().build();
         BuildConfiguration buildConfiguration = BuildConfiguration.Builder.newBuilder().id(0).scmRepoURL("https://github.com/project-ncl/pnc")
                 .buildScript("mvn install").name("MYBUILDCONFIG").environment(env).scmRevision("80f8fcf4e334daeef15667ccf1d4517f1050545b").build();
@@ -26,18 +28,20 @@ public class LocalBuildRunner {
         try {
             buildCoordinator.build(buildConfiguration, user);
         } catch (CoreException e) {
-            result = 1;
+            e.printStackTrace();
         }
+    }
 
-        while(!buildCoordinator.getBuildTasks().isEmpty()){
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                result = 1;
-            }
+    public void collectBuildStatusChangedEvent(@Observes BuildStatusChangedEvent buildStatusChangedEvent) {
+        if (buildCoordinator.getBuildTasks().isEmpty() && buildStatusChangedEvent.getNewStatus().isCompleted()){
+            System.exit(0);
         }
-        System.exit(result);
+    }
 
+    public void collectBuildSetStatusChangedEvent(@Observes BuildSetStatusChangedEvent buildSetStatusChangedEvent) {
+        if (buildCoordinator.getBuildTasks().isEmpty() && buildSetStatusChangedEvent.getNewStatus().isCompleted()){
+            System.exit(0);
+        }
     }
 
     public static void main(String[] args){
