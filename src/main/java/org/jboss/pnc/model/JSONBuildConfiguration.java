0c365c744edb0a41ad3b217f5d31756a41d64290
Ahmed Abu Lawi <aabulawi@redhat.com>
2015-07-07 15:31:18 -0400

    Use jackson library to convert JSON files into java objects.
    Tests included.

---
+++ src/main/java/org/jboss/pnc/model/JSONBuildConfiguration.java
@@ -0,0 +1,47 @@
+package org.jboss.pnc.model;
+
+import java.util.List;
+
+/**
+ * Created by aabulawi on 07/07/15.
+ */
+public class JSONBuildConfiguration {
+
+    private String name;
+    private String scm;
+    private String command;
+    private List<String> dependencies;
+
+    public String getName(){
+        return name;
+    }
+
+    public String getScm(){
+        return scm;
+    }
+
+    public String getCommand(){
+        return command;
+    }
+
+    public List<String> getDependencies(){
+        return dependencies;
+    }
+
+    public void setName(String name){
+         this.name = name;
+    }
+
+    public void getScm(String scm){
+        this.scm = scm;
+    }
+
+    public void getCommand(String command){
+        this.command = command;
+    }
+
+    public void getDependencies(List<String> dependencies){
+        this.dependencies = dependencies;
+    }
+
+}
