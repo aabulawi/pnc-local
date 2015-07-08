ea2b5e1b34c32e43e672d2d850e407e4cd65a823
Ahmed Abu Lawi <aabulawi@redhat.com>
2015-07-08 14:02:48 -0400

    Use args4j library parse command line arguments. Tests included.

--- src/main/java/org/jboss/pnc/LocalBuildRunner.java
+++ src/main/java/org/jboss/pnc/LocalBuildRunner.java
@@ -3,15 +3,20 @@ package org.jboss.pnc;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jboss.pnc.core.builder.BuildCoordinator;
 import org.jboss.pnc.core.exception.CoreException;
 import org.jboss.pnc.exception.IncompleteConfigurationException;
 import org.jboss.pnc.model.*;
+import org.jboss.pnc.parser.PncLocalCliOptions;
 import org.jboss.pnc.spi.events.BuildSetStatusChangedEvent;
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
 
 import java.io.*;
 import java.util.*;
@@ -53,27 +58,33 @@ public class LocalBuildRunner {
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        if (args.length == 0){
-            System.out.println("Please provide the path to a build.json");
-            return;
-        }
+        final PncLocalCliOptions options = new PncLocalCliOptions();
+        final CmdLineParser parser = new CmdLineParser(options);
 
-        Weld weld = new Weld();
-        WeldContainer weldContainer = weld.initialize();
-
-        File jsonSource = new File(args[0]);
-
-        ObjectMapper mapper = new ObjectMapper();
         try {
+            parser.parseArgument(args);
+
+            Weld weld = new Weld();
+            Runtime.getRuntime().addShutdownHook(new ShutdownHook(weld));
+            WeldContainer weldContainer = weld.initialize();
+
+            File jsonSource = options.getBuildJSON();
+            ObjectMapper mapper = new ObjectMapper();
             List<JSONBuildConfiguration> jsonConfig = mapper.readValue(jsonSource, new TypeReference<List<JSONBuildConfiguration>>() {});
+
             weldContainer.instance().select(LocalBuildRunner.class).get().run(jsonConfig);
+
+        } catch (CmdLineException e) {
+            parser.printUsage(System.err);
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (JsonParseException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        Runtime.getRuntime().addShutdownHook(new ShutdownHook(weld));
     }
 
     static class ShutdownHook extends Thread {
---
+++ src/test/java/org/jboss/pnc/parser/PncLocalCliOptionsTest.java
@@ -0,0 +1,34 @@
+package org.jboss.pnc.parser;
+
+import org.junit.Test;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by aabulawi on 08/07/15.
+ */
+public class PncLocalCliOptionsTest {
+
+    @Test
+    public void testValidArgs() throws CmdLineException {
+        String[] args = {"-f", "build.json"};
+        PncLocalCliOptions pncLocalCliOptions = new PncLocalCliOptions();
+        CmdLineParser cmdLineParser = new CmdLineParser(pncLocalCliOptions);
+        cmdLineParser.parseArgument(args);
+        assertEquals(new File("build.json"), pncLocalCliOptions.getBuildJSON());
+
+    }
+
+    @Test(expected = CmdLineException.class)
+    public void testInvalidArgs() throws CmdLineException {
+        String[] args = {"-t", "build.json"};
+        PncLocalCliOptions pncLocalCliOptions = new PncLocalCliOptions();
+        CmdLineParser cmdLineParser = new CmdLineParser(pncLocalCliOptions);
+        cmdLineParser.parseArgument(args);
+    }
+
+}
