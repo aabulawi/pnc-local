f4c40b3978b5d0a821990fb14f7d8361b4613b6b
Ahmed Abu Lawi <aabulawi@redhat.com>
2015-07-13 15:59:58 -0400

    Use maven-repository-manager

--- src/main/java/org/jboss/pnc/BuildConfigurationFactory.java
+++ src/main/java/org/jboss/pnc/BuildConfigurationFactory.java
@@ -3,6 +3,7 @@ package org.jboss.pnc;
 import org.jboss.pnc.model.BuildConfiguration;
 import org.jboss.pnc.model.Environment;
 import org.jboss.pnc.model.JSONBuildConfiguration;
+import org.jboss.pnc.model.Project;
 import org.jboss.pnc.source_manager.SourceManagerUtils;
 
 import java.util.Set;
@@ -15,7 +16,7 @@ public class BuildConfigurationFactory {
     public static BuildConfiguration createBuildConfigFromJSONConfig(int id, JSONBuildConfiguration config, Set<BuildConfiguration> dependencies){
         return BuildConfiguration.Builder.newBuilder().id(id).name(config.getName()).scmRevision(SourceManagerUtils.extractRevision(config.getScm()))
                 .scmRepoURL(SourceManagerUtils.extractUrl(config.getScm())).dependencies(dependencies).buildScript(config.getCommand())
-                .environment(Environment.Builder.defaultEnvironment().build()).build();
+                .environment(Environment.Builder.defaultEnvironment().build()).project(Project.Builder.newBuilder().name("Local Build - " + id).build()).build();
     }
 
 
